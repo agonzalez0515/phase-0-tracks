f1f090c019e691a9eabfa20fbff0f18cede9539f
agonzalez0515 <agonzalez0515@gmail.com>
2016-09-14 15:38:44 -0700

    update user inputs to register as correct data type

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -6,19 +6,30 @@ def hamster_survey
 		end
 
 	puts "How loud is you hamster (from 1 to 10)?"
-	volume = gets.chomp
+	volume = gets.chomp.to_i
+	# volume.to_i
 
 	puts "what color fur?"
 	fur_color = gets.chomp
 
-	puts "Is your hamster a good candidate for adoption?"
+	puts "Is your hamster a good candidate for adoption (y/n)?"
 	good_candidate = gets.chomp
+		if good_candidate == y
+			good_candidate = true
+
+		elsif good_candidate == n
+			good_candidate = false
+
+		else 
+			good_candidate = nil
+		end
+
 
 	puts "Estimated age of hamster?"
-	estimated_age = gets.chomp
+	estimated_age = gets.chomp.to_f
 		if estimated_age == ""
 			estimated_age = nil
 		end
 end
 
-
+hamster_survey 
