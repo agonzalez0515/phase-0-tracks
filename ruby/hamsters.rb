8d9c8dea355fa65a8e749edc6446cc520f76c719
agonzalez0515 <agonzalez0515@gmail.com>
2016-09-14 15:57:07 -0700

    update printed messages so blank variables don't print

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -15,10 +15,10 @@ def hamster_survey
 
 	puts "Is your hamster a good candidate for adoption (y/n)?"
 	good_candidate = gets.chomp
-		if good_candidate == y
+		if good_candidate == "y"
 			good_candidate = true
 
-		elsif good_candidate == n
+		elsif good_candidate == "n"
 			good_candidate = false
 
 		else 
@@ -27,17 +27,18 @@ def hamster_survey
 
 
 	puts "Estimated age of hamster?"
-	estimated_age = gets.chomp.to_f
+	estimated_age = gets.chomp
 		if estimated_age == ""
 			estimated_age = nil
 		end
 
+	estimated_age.to_f
 
 	puts "The hamster's name is #{name}."
 	puts "The hamster is #{volume} level."
 	puts "It's fur color is #{fur_color}."
-	unless good_candidate == nil puts "The hamster is a good candidate? #{good_candidate}"
-	puts "The estimated age of the hamster is #{estimated_age}."
+	puts "The hamster is a good candidate? #{good_candidate}" unless good_candidate == nil 
+	puts "The estimated age of the hamster is #{estimated_age}." unless estimated_age == nil
 end
 
 hamster_survey 
