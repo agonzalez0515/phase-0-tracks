2ed992d7c2286ad41ec8f81b2aa7c9185c49698d
agonzalez0515 <agonzalez0515@gmail.com>
2016-09-15 22:09:53 -0700

    create loop question about allergies

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -6,9 +6,9 @@
 	def vampire_test
 		puts "How many employees will be processed?"
 		number_of_employees = gets.chomp.to_i
-		until
-		number_of_employees == 0
-		number_of_employees = number_of_employees - 1
+		
+		while number_of_employees > 0
+			number_of_employees = number_of_employees - 1
 		
 
 		puts "What is your name?"
@@ -30,16 +30,33 @@
 					wants_garlic_bread = false
 				end
 
-		puts "would you like to enroll in the company's health insurance?"
+		puts "would you like to enroll in the company's health insurance? (y/n)"
 			wants_health_insurance = gets.chomp
 				if wants_health_insurance == "y"
 					wants_health_insurance = true
 				else 
 					wants_health_insurance = false
 				end
-
 		
-			if name == "Drake Cula" || name == "Tu Fang"
+
+			allergies = " "
+			puts "Please list all allergies. Type done when finished."
+
+			until allergies == "done" || allergies == "sunshine"
+			allergies = gets.chomp
+			end
+
+				if allergies == "sunshine"
+					allergies = true
+				else
+					allergies = false 	
+				end
+
+			
+			if allergies 
+				puts "Probably a vampire"
+
+				elsif name == "Drake Cula" || name == "Tu Fang"
 				puts "definitely a vampire"
 
 				elsif age + year_born == 2016 && (wants_garlic_bread || wants_health_insurance)
@@ -59,8 +76,7 @@
 		
 						#prints employee answers
 						puts "The employee's name is #{name}."
-						puts "The employee is #{age} years old."
-						puts "The employee was born in the year #{year_born}."
+						puts "They are #{age} years old and born in the year #{year_born}."
 						puts "Does the employee want garlic bread? #{wants_garlic_bread}."
 						puts "Does the employee want health insurance? #{wants_health_insurance}."
 		
