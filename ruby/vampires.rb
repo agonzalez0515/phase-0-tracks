4792a0844a470a483549491ef5ac7f2e1f7c3337
agonzalez0515 <agonzalez0515@gmail.com>
2016-09-15 21:15:09 -0700

    add loop to method to account for multiple employees in one day

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,63 +1,73 @@
 #ask questions to employees
 
 
-def vampire_test
-	puts "What is your name?"
-		name = gets.chomp
-
-	puts "How old are you?" 
-		age = gets.chomp.to_i
 
 
-	puts "What year were you born?"
-		year_born = gets.chomp.to_i
+	def vampire_test
+		puts "How many employees will be processed?"
+		number_of_employees = gets.chomp.to_i
+		until
+		number_of_employees == 0
+		number_of_employees = number_of_employees - 1
+		
+
+		puts "What is your name?"
+			name = gets.chomp
+
+		puts "How old are you?" 
+			age = gets.chomp.to_i
 
 
-	puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
-		wants_garlic_bread = gets.chomp
-			if wants_garlic_bread == "y"
-				wants_garlic_bread = true
-			else 
-				wants_garlic_bread = false
-			end
+		puts "What year were you born?"
+			year_born = gets.chomp.to_i
 
-	puts "would you like to enroll in the company's health insurance?"
-		wants_health_insurance = gets.chomp
-			if wants_health_insurance == "y"
-				wants_health_insurance = true
-			else 
-				wants_health_insurance = false
-			end
 
-	
-	if name == "Drake Cula" || name == "Tu Fang"
-		puts "definitely a vampire"
+		puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+			wants_garlic_bread = gets.chomp
+				if wants_garlic_bread == "y"
+					wants_garlic_bread = true
+				else 
+					wants_garlic_bread = false
+				end
 
-		elsif age + year_born == 2016 && (wants_garlic_bread || wants_health_insurance)
-		puts "Probably not a vampire"
-
-		elsif age + year_born != 2016 && (wants_garlic_bread || wants_health_insurance)
-		puts "Probably a vampire"
-
-		elsif age + year_born != 2016 && !(wants_garlic_bread && wants_health_insurance)
-		puts "Almost definitely a vampire"
-
-		else
-			puts "Results inconclusive"
+		puts "would you like to enroll in the company's health insurance?"
+			wants_health_insurance = gets.chomp
+				if wants_health_insurance == "y"
+					wants_health_insurance = true
+				else 
+					wants_health_insurance = false
+				end
 
 		
+			if name == "Drake Cula" || name == "Tu Fang"
+				puts "definitely a vampire"
+
+				elsif age + year_born == 2016 && (wants_garlic_bread || wants_health_insurance)
+				puts "Probably not a vampire"
+
+				elsif age + year_born != 2016 && (wants_garlic_bread || wants_health_insurance)
+				puts "Probably a vampire"
+
+				elsif age + year_born != 2016 && !(wants_garlic_bread && wants_health_insurance)
+				puts "Almost definitely a vampire"
+
+				else
+					puts "Results inconclusive"
+
+				
+			end
+		
+						#prints employee answers
+						puts "The employee's name is #{name}."
+						puts "The employee is #{age} years old."
+						puts "The employee was born in the year #{year_born}."
+						puts "Does the employee want garlic bread? #{wants_garlic_bread}."
+						puts "Does the employee want health insurance? #{wants_health_insurance}."
+		
+
+		end
 	end
 
-
-
-puts "The employee's name is #{name}"
-puts "The employee is #{age} years old"
-puts "The employee was born in the year #{year_born}"
-puts "Does the employee want garlic bread? #{wants_garlic_bread}"
-puts "Does the employee want health insurance? #{wants_health_insurance}"
-
-
-end
 
 vampire_test
 
