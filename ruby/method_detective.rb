252b5c0c1989a23b7229376edfbddcf741ebdfc6
agonzalez0515 <agonzalez0515@gmail.com>
2016-09-19 20:28:41 -0700

    add squeeze and count classes

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -30,15 +30,15 @@ a << " suspects"
 "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+# "The mystery of the missing first letter".delete[0]
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+"Elementary,    my   dear        Watson!".squeeze
 # => "Elementary, my dear Watson!"
 
 # "z".<???>
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+"How many times does the letter 'a' appear in this string?".count "a"
 # => 4
