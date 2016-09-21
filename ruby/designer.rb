220b6f01ce112eb08f8d49cfea8a7bb45d653182
agonzalez0515 <agonzalez0515@gmail.com>
2016-09-21 12:58:22 -0700

    create hash code

---
+++ ruby/designer.rb
@@ -0,0 +1,21 @@
+#Interior Designer Job Application
+
+ applicant = {
+ 	name: "Angie Gonzalez",
+ 	address: "123 Main St",
+ 	email: "email@email,com",
+ 	phone: 555-555-5555,
+ 	fav_blue: "cerulean",
+ 	wallpaper_pref: "paisley",
+ 	ombre: "so last season"
+ }
+
+
+applicant[:hired] = "yes" 
+applicant[:phone] = "444-444-4444"
+
+
+applicant[:name] + applicant[:address]
+
+
+p applicant
