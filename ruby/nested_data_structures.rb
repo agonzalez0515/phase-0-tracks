38fc109a026f9f1bd762c045a33489bb8b0b0e3f
agonzalez0515 <agonzalez0515@gmail.com>
2016-09-22 17:02:03 -0700

    create nested data structure for WWE Show

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,49 @@
+wwe_show = {
+	in_ring: {
+		wrestler_1: 'Shawn Michaels',
+		wrestler_2: 'The Undertaker',
+		referee: 'Chris Robinson',
+		commentators: [
+			'Jim Ross', 
+			'Jerry Lawler', 
+			'Michael Cole'
+		]
+	}
+
+	audience: {
+		max_capacity: 1000,
+		attendance: 890,
+	}
+
+	backstage: {
+		catering: [
+			'burgers',
+			'green_salad',
+			'fruit_salad',
+			'protein_bars'
+		]
+		locker_room: {
+			men_capacity: 50,
+			women_capacity:35,
+		}
+	
+	crew: {
+		producers: [
+			'Triple H'
+			'Vince McMahon'
+			'Steph McMahon'
+		]
+		cameras: {
+			cameramen:[
+				'Seth Green'
+				'Kevin Dunn'
+				"Freddie Prinze"
+			]
+			total_cameras: 5,
+			total_mics: 15,
+			total_crew: 100,
+		}	
+
+	}		
+
+}
