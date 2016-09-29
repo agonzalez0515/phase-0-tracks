694eba058a77a4b8e46e8e4dc77034e083823807
agonzalez0515 <agonzalez0515@gmail.com>
2016-09-29 16:59:15 -0700

    create array with instances of the class, iterate through each instance with instance methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -20,11 +20,54 @@ class Puppy
 		dog_age = human_years * 7
 	end
 
+	def cuddle
+		puts "*cuddles*"
+	end
+
 end
 
+=begin
 fido = Puppy.new
 
 puts fido.fetch("stick")
 puts fido.speak(3)
 puts fido.roll_over
 puts fido.dog_years(2)
+puts fido.cuddle
+=end
+
+class DBC
+
+	def initialize
+		puts "Initializing DBC method.. time for another pairing!"
+	end
+
+	def refactor
+		puts "You need to refactor your code!"
+	end
+
+	def feedback(x)
+		if x==true
+			puts "Awesome work!"
+		else
+			puts "No bueno :("
+		end
+	end
+end
+
+
+new_dbc = []
+
+50.times do |x|
+	x=DBC.new
+	new_dbc<<x
+end
+
+#p new_dbc
+
+#new_dbc[0].refactor
+
+new_dbc.each do |item|
+	item.refactor
+	item.feedback(false)
+end
