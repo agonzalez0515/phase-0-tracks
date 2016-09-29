b9c0cec82c3faee0b0111b234cd4eeb7bb952cbd
agonzalez0515 <agonzalez0515@gmail.com>
2016-09-29 16:24:56 -0700

    add methods to release 0 for festch, speak, roll over and human years

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -2,16 +2,29 @@ class Puppy
 
   def fetch(toy)
     puts "I brought back the #{toy}!"
-    toy
   end
 
   def initialize
 		puts "Initializing Puppy instance..."
-		@toy = stick
+	end
+
+	def speak(i)
+		puts "woof! " * i
+	end
+
+	def roll_over
+		puts "*roll over*"
+	end
+
+	def dog_years(human_years)
+		dog_age = human_years * 7
 	end
 
 end
 
 fido = Puppy.new
 
-puts fido.fetch
+puts fido.fetch("stick")
+puts fido.speak(3)
+puts fido.roll_over
+puts fido.dog_years(2)
