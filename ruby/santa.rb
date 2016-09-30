c87c1014e49ebde383bd43cdff3fd3c998130769
agonzalez0515 <agonzalez0515@gmail.com>
2016-09-30 11:01:06 -0700

    add random_santas method and loop 5 times for testing

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,15 +1,13 @@
-class Santa
+class Santa 
+	attr_reader :age
+	attr_accessor :gender, :ethnicity
 
-	attr_reader :age, :ethnicity
-	attr_accessor :gender 
-
-	def initialize(gender, ethnicity)
+	def initialize
 		@gender = gender
 		@ethnicity = ethnicity
 		@age = 0
 		puts "Initializing Santa instance..."
 	end
-
 
 	def speak
 		puts "ho, ho, ho! Haaaappy holidays!"
@@ -34,41 +32,46 @@ class Santa
 		reindeer_ranking.insert(-1, reindeer_ranking.delete_at(index))
 	end
 
-	
-
-	def gender=(new_gender)
-		@gender = new_gender
+	def random_santas
+		example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A", "trans", "non-binary"]
+		example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A", "native-american", "pacic islander"]
+		@gender = example_genders.sample
+		@ethnicity = example_ethnicities.sample
 	end
-
 	
-
-
-
 end
 
 
 #Driver Code
-santacon = Santa.new("female", "latina")
-santacon.celebrate_birthday
+#santacon = Santa.new("female", "latina")
+#santacon.celebrate_birthday
 #puts santacon.speak
 #puts santacon.eat_milk_and_cookies("oreo")
 
 
 #p santacon.get_mad_at("Rudolph")
 
-p santacon.gender
-santacon.gender = "male"
-puts "This Santa's gender is #{santacon.gender}"
+#p santacon.gender
+#santacon.gender = "male"
+#puts "This Santa's gender is #{santacon.gender}"
 
-puts "This santa is #{santacon.age} years old."
-puts "This sants identifies as #{santacon.ethnicity}."
+#puts "This santa is #{santacon.age} years old."
+#puts "This sants identifies as #{santacon.ethnicity}."
 
-#santas = []
 
-#santas << Santa.new("female", "latina")
-#santas << Santa.new("male", "white")
-#santas << Santa.new("non-binary", "japanese")
-#santas << Santa.new("trans", "native-american")
-#p santas
+#Release 4
+
+5.times do
+
+	my_santa = Santa.new
+	my_santa.random_santas
+	puts "This santa's gender is #{my_santa.gender} and ethnicity is #{my_santa.ethnicity}."
+	puts "_" * 15 
+end
+
+
+
+
+
 
 
