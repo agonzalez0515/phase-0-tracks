17d8f2ef71516300f780ebb5ac5aefc3d5656b08
agonzalez0515 <agonzalez0515@gmail.com>
2016-10-03 08:59:36 -0700

    another attempt at making the user_guess method work. Still has bug

--- ruby/word_game.rb
+++ ruby/word_game.rb
@@ -1,54 +1,64 @@
-#enter word
+#take 1 word in at the start
 #determine word length and set it equal to guess count
-#split the word into array
+#split the word into secret word array
 #ask user 2 for letter guess
 #store guess in a different array
 #iterate through the word to see if letter include?
 #if include? print letter in correct spot
 #if !include? ask for another guess and subract 1 from word count
-#end game when word count == 0
-#secret_word = ["u", "n", "i", "c", "o", "r", "n"]
+#end game when word count == 0 or word is guessed
+
 
 
 class Word_game
-	attr_reader :guess_count, :word
-
+	attr_reader :word, :progress
+	attr_accessor :guess_count
 
 	def initialize(word) 
-    @guess_count = 0
-    @word = word
-    @secret_word = word.split(//)
+    	@word = word
+    	@guess_count = @word.length
+    	@secret_word = word.split(//)
+    	@progress = ""
   	end
 
-	def guesses
-		@guess_count = @word.length
-	end
-
 	
+
+	#insert the guessed letter into the corresponding part of the new "progress" word.
+	#as it is now, it inserts it either into every spot because of .each or inserts letter
+	#duplicated if guessed 2 times. no good.
 	def user_guess(letter)
 		index = 0
-		progress = ""
-		@secret_word.each do |l|
-			if letter.include? l
-				progress << letter[index]
-			else
-				progress << "-"
+		#@secret_word.each do |l|
+			if @secret_word.include? letter 
+				@progress.insert(@secret_word.index(letter), letter)
+				else
+				@progress << "-"
 			end
-		end
-	puts "This is your word so far:#{progress}"
+		#end
+	puts "Your word so far: #{@progress}"	
+	@progress
 	end
 
 	def counter(letter)
-		if @secret_word.include? letter
-			@guess_count
-		else
-			@guess_count -=1
+		if !@secret_word.include? letter
+				@guess_count -=1
 		end
-		print "You have #{@guess_count} guesses left."
+	end
+
+	#doesn't work because if I call it, it will print either option.
+	def game_over
+		if @guess_count == 0
+			puts "you suck. bye."
+		elsif
+			@progress == @word
+			puts "You won!!!"
+			return
+		else
+
+		end
 	end
 
 end
-
 
 
 
@@ -62,41 +72,20 @@ puts "Player 1, what's the secret word?"
 word = gets.chomp.to_s
 game_1 = Word_game.new(word)
 
-game_1.guesses
+game_1.guess_count
 puts "Player 2, you have #{game_1.guess_count} guesses."
+puts "Word is:" 
+puts "-" * game_1.guess_count
 puts "Ready? Let's play!"
 
-while game_1.guesses > 0
+while game_1.guess_count > 0
 	puts "Please enter a letter"
 	letter = gets.chomp.to_s
 	game_1.user_guess(letter)
 	game_1.counter(letter)
+	puts "You have #{game_1.guess_count} guesses left."
 end
 
 
 
 
-
-
-=begin
-while !game.is_over
-  puts "Which cup has the ball? Enter a guess of 1, 2, or 3:"
-  guess = gets.chomp.to_i
-  if !game.check_cup(guess - 1)
-  	puts "Nope! Try again."
-  end
-end
-
-puts "You won in #{game.guess_count} guesses!"
-
-game_1 = Word_game.new("unicorn")
-p game_1.guesses
-p game_1.word_to_guess
-game_1.user_guess("u")
-p game_1.user_guess("b")
-p game_1.guess_count
-p game_1.user_guess("c")
-p game_1.guess_count
-p game_1.user_guess("a")
-p game_1.guess_count
-=end
