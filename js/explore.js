c13f0430b720c76f40648a6eda882be94235c527
agonzalez0515 <agonzalez0515@gmail.com>
2016-10-04 20:33:25 -0700

    create horses object combines two arrays

---
+++ js/explore.js
@@ -0,0 +1,19 @@
+//Pseudocode for reverse string javascript
+
+//for each letter in the string, pick up the last one and
+//then print it until you have gone through the whole string length
+//store each printed letter as a new string
+//join the strings to equal one word
+
+
+
+
+function reverse(string) {
+	for (var i = string.length; i > -1; i --){
+		(string[i]);
+		
+	}
+}
+
+var x = reverse("hello")
+console.log(var x)
