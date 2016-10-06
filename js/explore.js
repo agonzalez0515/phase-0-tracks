e18ec595b015a3691373f8252c1485dd474f6f01
agonzalez0515 <agonzalez0515@gmail.com>
2016-10-05 19:57:47 -0700

    write pseudocode for js reverse function

---
+++ js/explore.js
@@ -0,0 +1,11 @@
+//Pseudocode for reverse string javascript
+
+//for each letter in the string, pick up the last one and
+//then print it until you have gone through the whole string length
+//store each printed letter as a new string
+//join the strings to equal one word
+
+//^ this didn't work^
+
+
+
