6d523a91649a49885cff12307714bdebe5dc59e7
agonzalez0515 <agonzalez0515@gmail.com>
2016-10-07 12:02:24 -0700

    work on release 2. create method that returns random word with random number of letters.

--- js/algos.js
+++ js/algos.js
@@ -25,9 +25,55 @@ function biggestword(words){
 
 
 //Release 1
-//Pseudocode
+//PSEUDOCODE
 //
 
+
+
+function keymatch (array){
+
+
+  if (array[0].name == array[1].name){
+			console.log("true");
+	}
+	else if (array[0].age == array[1].age) {
+		console.log("true");
+	}
+	else {
+		console.log("false");
+	} 
+}
+
+
+var example = [{name: "Steven", age: 6},{name: "Steven", age: 54}];
+
+keymatch(example);
+
+
+
+//Release2
+//PSEUDOCODE
+//make an array of the alphabet
+//generate a random number using 1-10 range. this is the length of the word.
+//loop through the alphabet that many times and use another random number (from 1-26) as the index of the letter you want.
+//push that letter into a new array
+//join the array to make a string
+//push string into a word array
+//loop through the whole function the number of times the paramater was
+//print/return? final word array with all the words
+
+
+
+RandomLength = Math.floor(Math.random() * 10) + 1; //random number to determine length of random word
+console.log(RandomLength);
+var RandomLetters = [];
+
+for (var i = 0; i < RandomLength; i++){
+	var alphabet = "abcdefghijklmnopqrstuvwxyz";
+	RandomLetters.push(alphabet[Math.floor(Math.random() * 26) + 1]);
+	}
+	RandomLetters.join("");
+	console.log(RandomLetters)
 
 
 //DRIVER CODE 
