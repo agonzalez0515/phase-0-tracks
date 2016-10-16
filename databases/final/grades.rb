51fc91cd798693b4859463281303cf6dd084bacf
agonzalez0515 <agonzalez0515@gmail.com>
2016-10-15 18:17:25 -0700

    Create and call 30 times method to generate random students and their grades.

--- databases/final/grades.rb
+++ databases/final/grades.rb
@@ -2,8 +2,13 @@ require 'sqlite3'
 require 'faker'
 
 
+#create database 
 db = SQLite3::Database.new("student_grades.db")
 
+#creates database information as hash. I can access keys by their name
+db.results_as_hash = true
+
+#delimiter to make a table that has each student's name and their scores
 create_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS students(
     id INTEGER PRIMARY KEY,
@@ -15,6 +20,15 @@ create_table_cmd = <<-SQL
   );
 SQL
 
+#create students table
 db.execute(create_table_cmd)
 
-db.execute("INSERT INTO students (name, essay_1, essay_2, midterm_1, midterm_2) VALUES ('Captain', 60, 50, 70, 65);")
+#method to generate students with 4 scores each
+def create_students (db, name, essay_1, essay_2, midterm_1, midterm_2)
+	db.execute("INSERT INTO students (name, essay_1, essay_2, midterm_1, midterm_2) VALUES (?, ?, ?, ?, ?)", [name, essay_1, essay_2, midterm_1, midterm_2])
+end
+
+#loop that calls method to create a student roster with grades. the names and grades are random.
+30.times do
+  create_students(db, Faker::Name.name, rand(40..100), rand(40..100), rand(40..100), rand(40..100))
+end
