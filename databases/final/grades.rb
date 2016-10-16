b696951a1f56dfd867194954ce7a2e0dc8b7789f
agonzalez0515 <agonzalez0515@gmail.com>
2016-10-16 00:22:51 -0700

    finish user interface by adding a password and a loop to teacher section

--- databases/final/grades.rb
+++ databases/final/grades.rb
@@ -29,6 +29,16 @@ def create_students (db, name, essay_1, essay_2, midterm_1, midterm_2)
 	db.execute("INSERT INTO students (name, essay_1, essay_2, midterm_1, midterm_2) VALUES (?, ?, ?, ?, ?)", [name, essay_1, essay_2, midterm_1, midterm_2])
 end
 
+
+# loop that calls method to create a student roster with grades. The names and grades are random.
+# This is just to start the list. Teacher can add information later. Commented out after running it once, 
+# or else it would keep adding 30 students everytime the program runs.
+
+#30.times do
+#  create_students(db, Faker::Name.name, rand(40..100), rand(40..100), rand(40..100), rand(40..100))
+#end
+
+
 # delete a student
 def delete_students(db, student_name)
 	db.execute("DELETE FROM students WHERE name=?", [student_name])
@@ -42,15 +52,6 @@ end
 
 
 
-# loop that calls method to create a student roster with grades. The names and grades are random.
-# This is just to start the list. Teacher can add information later. Commented out after running it once, 
-# or else it would keep adding 30 students everytime the program runs.
-
-#30.times do
-#  create_students(db, Faker::Name.name, rand(40..100), rand(40..100), rand(40..100), rand(40..100))
-#end
-
-
 roster = db.execute("SELECT * FROM students")
 
 # calculates final grade by taking the average of 4 grades and creates new array
@@ -58,8 +59,6 @@ final_grades =[]
 roster.each do |student|
 	final_grades << (student['essay_1'] + student['essay_2'] + student['midterm_1'] + student['midterm_2'])/4
 end
-
-#p final_grades
 
 
 # creates hash with key student name from roster table and value of final grade from array
@@ -73,6 +72,16 @@ end
 
 # hash with each students' final grade
 final_student
+
+# prints a nicer way to read the roster
+def print_grades (roster, final_student)
+	roster.each do |student|
+		puts "#{student['name']} has these grades: #{student['essay_1']}, #{student['essay_2']}, #{student['midterm_1']}, #{student['midterm_2']}."
+	end
+	final_student.each do |student, grade|
+		puts "#{student}'s final grade is #{grade}."
+	end
+end
 
 
 # method for students to check their grade
@@ -88,7 +97,7 @@ end
 
 
 ##User Interface##
-
+puts "Welcome!"
 puts "Are you a teacher or student?"
 answer = gets.chomp
 if answer == "student"
@@ -99,47 +108,61 @@ if answer == "student"
 	student_name = gets.chomp
 	check_grades(student_name, final_student)
 else
-	puts "Please pick an option number."
-	puts "1: Add student"
-	puts "2: Delete student"
-	puts "3: Check student"
-	puts "4: Print all grades"
-	puts "5: Quit"
-	option = gets.chomp.to_i
-		
-			if option == 1
-				puts "Enter full name:"
-				name = gets.chomp
-				puts "Enter Essay 1 score"
-				essay_1 = gets.chomp
-				puts "Enter Essay 2 score"
-				essay_2 = gets.chomp
-				puts "Enter Midterm 1 score"
-				midterm_1 = gets.chomp
-				puts "Enter Midterm 2 score"
-				midterm_2 = gets.chomp
-				create_students(db, name, essay_1, essay_2, midterm_1, midterm_2)
-				puts "Thank you. The student has been added to the database."
-			elsif option == 2
-				puts "Please enter the name of the student you would like to delete."
-				delete_name = gets.chomp
-				delete_students(db, delete_name)
-				puts "Thank you. The student has been removed from the database."
-			elsif option == 3
-				puts "Please enter the name of the student you would like to check."
-				name_check = gets.chomp
-				check_student(db, name_check, final_student)
-			elsif option == 4
-				puts final_student
-				
-			else option == 5
-				puts "Thank you for being an awesome teacher!"
-			end	
+	
+	puts "Please enter password:"
+		password = gets.chomp
+		if password == "ILOVEMATH"
+			valid_input = false
+			until valid_input
+			puts "Please pick an option number."
+			puts "1: Add student"
+			puts "2: Delete student"
+			puts "3: Check student"
+			puts "4: Print all grades"
+			puts "5: Quit"
+			option = gets.chomp.to_i
+				if option == 1
+					puts "Enter student's full name:"
+					name = gets.chomp
+					puts "Enter Essay 1 score"
+					essay_1 = gets.chomp
+					puts "Enter Essay 2 score"
+					essay_2 = gets.chomp
+					puts "Enter Midterm 1 score"
+					midterm_1 = gets.chomp
+					puts "Enter Midterm 2 score"
+					midterm_2 = gets.chomp
+					create_students(db, name, essay_1, essay_2, midterm_1, midterm_2)
+					puts "Thank you. The student has been added to the database."
+				elsif option == 2
+					puts "Please enter the name of the student you would like to delete."
+					delete_name = gets.chomp
+					delete_students(db, delete_name)
+					puts "Thank you. The student has been removed from the database."
+				elsif option == 3
+					puts "Please enter the name of the student you would like to review."
+					name_check = gets.chomp
+					check_student(db, name_check, final_student)
+				elsif option == 4
+				 	print_grades(roster, final_student)
+				else option == 5
+					valid_input = true
+					puts "Thank you for being an awesome teacher!"
+					puts "Have a nice day."
+				end
+			end
+		else
+			puts "Incorrect password. Bye!"
+		end	
 end
 
 
 
-
-
+=begin
+NOTES/NEED TO REFACTOR	
+- can I make a new table for the final grades and then join it with the first table to print a more concise report?
+-
+	
+=end
 
 
