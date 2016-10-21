026e8422ea6a4dc48b90b8ca0edca411a82a0a30
agonzalez0515 <agonzalez0515@gmail.com>
2016-10-21 15:02:17 -0700

    received files from pair and rafactor release 2

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -17,11 +17,25 @@ get '/students/new' do
   erb :new_student
 end
 
+get '/students/delete' do
+	erb :delete
+end
+
 # create new students via
 # a form
 post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
+end
+
+post '/student' do
+	db.execute("DELETE FROM students WHERE name=?", [params['name']])
+	redirect '/'
+end
+
+post '/student_id' do
+	db.execute("DELETE FROM students WHERE id=?", [params['id']])
+	redirect'/'
 end
 
 # add static resources
